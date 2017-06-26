fdedc8aa063026a9c4164ec8f0678907995977f1
Ivan <iqsk81ad@gmail.com>
2017-06-26 12:06:20 +0300

    #u[date

--- public_html/wp-content/form_lab.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type:text/html;charset=utf-8");
-?>
-<form action="form_develop.php" method="post">
-<p>Введите размерность лабиринта</p>
-<input name="length" type="text" />
-<input name="" type="submit" />
-</form>
